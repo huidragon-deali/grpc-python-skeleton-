77499accb967c6a65efb5244a62ca663df8454ed
huidragon-deali <79898452+huidragon-deali@users.noreply.github.com>
2021-04-20 15:31:44 +0900

    interceptor

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import grpc_pb2
 import grpc_pb2_grpc
 
 
-def get_logger():
+def getLogger():
     __logger = logging.getLogger('logger')
     stream_handler = logging.StreamHandler()
     __logger.addHandler(stream_handler)
@@ -15,31 +15,38 @@ def get_logger():
     return __logger
 
 
+class Interceptor(grpc.ServerInterceptor):
+
+    def intercept_service(self, continuation, handler_call_details):
+        getLogger().debug(handler_call_details)
+        return continuation(handler_call_details)
+
+
 class GrpcService(grpc_pb2_grpc.GrpcServiceServicer):
 
     def getOne(self, request, context):
-        get_logger().debug(request)
+        getLogger().debug(request)
         return grpc_pb2.Response(value=[1.1, 2.2, 3.3])
 
     def serverStream(self, request, context):
-        get_logger().debug(request)
+        getLogger().debug(request)
         for i in range(1, 4):
             yield grpc_pb2.Response(value=[1.1 * i, 2.2 * i, 3.3 * i])
 
     def clientStream(self, request_iterator, context):
         for req in request_iterator:
-            get_logger().debug(req)
+            getLogger().debug(req)
         return grpc_pb2.Response(value=[1.1, 2.2, 3.3])
 
     def biStream(self, request_iterator, context):
         for req in request_iterator:
-            get_logger().debug(req)
+            getLogger().debug(req)
             for i in range(1, 4):
                 yield grpc_pb2.Response(value=[1.1 * i, 2.2 * i, 3.3 * i])
 
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), interceptors=(Interceptor(),))
     grpc_pb2_grpc.add_GrpcServiceServicer_to_server(GrpcService(), server)
     server.add_insecure_port('[::]:8888')
     server.start()
